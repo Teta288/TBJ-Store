db2a708155d24c67abe8e76779de49f9257f0219
Redeemed <cococe@Reedemed-MacbookPro.local>
2023-03-15 20:48:17 +0200

    changes in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cupertino_store/firebase_options.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
@@ -8,7 +9,9 @@ import 'model/app_state_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   return runApp(
     ChangeNotifierProvider<AppStateModel>(
       create: (_) => AppStateModel()..loadProducts(),
